e6b3eba5911e041f613afa5e4f5ffca2e1d7f14b
amr <amr.hedeiwy@gmail.com>
2025-01-13 14:43:12 +0200

    Wrapped EdgeLabelRenderer in foreignObject for browser support

--- src/app/workflow/_components/edges/DeletableEdge.tsx
+++ src/app/workflow/_components/edges/DeletableEdge.tsx
@@ -11,15 +11,15 @@ import {
   getSmoothStepPath,
   useReactFlow,
 } from "@xyflow/react";
-import React, { useState, useRef } from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 
 const DeletableEdge = (props: EdgeProps) => {
   const [edgePath, labelX, labelY] = getSmoothStepPath(props);
   const { setEdges } = useReactFlow();
 
-  const [isHovered, setIsHovered] = useState(false);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [isHovered, setIsHovered] = React.useState(false);
+  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
 
   const handleMouseEnter = () => {
     if (!timeoutRef.current) {
@@ -47,29 +47,31 @@ const DeletableEdge = (props: EdgeProps) => {
         markerEnd={props.markerEnd}
         style={props.style}
       />
-      <EdgeLabelRenderer>
-        <div
-          className={cn("transition-opacity duration-300", {
-            "pointer-events-none opacity-0": !isHovered,
-            "pointer-events-auto opacity-100": isHovered,
-          })}
-          style={{
-            position: "absolute",
-            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-          }}
-        >
-          <Button
-            variant="outline"
-            size="icon"
-            className="h-8 w-8 cursor-pointer rounded-full text-xs leading-none hover:shadow-lg"
-            onClick={() => {
-              setEdges((eds) => eds.filter((e) => e.id !== props.id));
+      <foreignObject>
+        <EdgeLabelRenderer>
+          <div
+            className={cn("transition-opacity duration-300", {
+              "pointer-events-none opacity-0": !isHovered,
+              "pointer-events-auto opacity-100": isHovered,
+            })}
+            style={{
+              position: "absolute",
+              transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
             }}
           >
-            <XIcon size={ICON_SIZE.XS} />
-          </Button>
-        </div>
-      </EdgeLabelRenderer>
+            <Button
+              variant="outline"
+              size="icon"
+              className="h-8 w-8 cursor-pointer rounded-full text-xs leading-none hover:shadow-lg"
+              onClick={() => {
+                setEdges((eds) => eds.filter((e) => e.id !== props.id));
+              }}
+            >
+              <XIcon size={ICON_SIZE.XS} />
+            </Button>
+          </div>
+        </EdgeLabelRenderer>
+      </foreignObject>
     </g>
   );
 };
